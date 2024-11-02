d3509bf3f43f3e803c3e02090c3d08fdc2950ed1
Md. Mehedi Hasan <mdmehedi17n@gmail.com>
2024-11-02 17:57:58 +0600

    OnboardingScreen & Font Added

--- app/src/main/java/com/example/nectar/modules/navigation/Navigation.kt
+++ app/src/main/java/com/example/nectar/modules/navigation/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.nectar.modules.login.LoginScreen
+import com.example.nectar.modules.onboarding.OnboardingScreen
 import com.example.nectar.modules.splash.SplashScreen
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.seconds
@@ -15,25 +16,33 @@ fun NavigationRoot() {
     // Nav host Composable
     val navController = rememberNavController()
 
-    //Nav graph
-    NavHost(navController = navController, startDestination = ScreenRoot.SplashScreen.router){
+    // Nav graph
+    NavHost(navController = navController, startDestination = ScreenRoot.SplashScreen.router) {
         composable(ScreenRoot.SplashScreen.router) {
-
-            LaunchedEffect(key1 = null) {
+            // LaunchedEffect to navigate after a delay
+            LaunchedEffect(Unit) {
                 delay(3.seconds)
-                navController.popBackStack()
-                navController.navigate(ScreenRoot.LoginScreen.router)
+                navController.navigate(ScreenRoot.OnboardingScreen.router) {
+                    // This ensures that the splash screen is removed from the back stack
+                    popUpTo(ScreenRoot.SplashScreen.router) { inclusive = true }
+                }
             }
-            SplashScreen()
+            SplashScreen() // Display the splash screen
         }
+
+        composable(ScreenRoot.OnboardingScreen.router) {
+            OnboardingScreen(navController) // Pass navController to the OnboardingScreen
+        }
+
         composable(ScreenRoot.LoginScreen.router) {
-            LoginScreen()
+            LoginScreen() // Display the login screen
         }
     }
 }
 
-sealed class ScreenRoot(val router: String){
-    object SplashScreen: ScreenRoot("splash")
-    object LoginScreen: ScreenRoot("login")
-
+// Define the navigation routes
+sealed class ScreenRoot(val router: String) {
+    object SplashScreen : ScreenRoot("splash")
+    object LoginScreen : ScreenRoot("login")
+    object OnboardingScreen : ScreenRoot("onboarding")
 }
--- app/src/main/java/com/example/nectar/ui/theme/Type.kt
+++ app/src/main/java/com/example/nectar/ui/theme/Type.kt
@@ -2,33 +2,61 @@ package com.example.nectar.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.nectar.R
+
+val gilroyFontFamily = FontFamily(
+    Font(R.font.gilroy_black, FontWeight.Black),
+    Font(R.font.gilroy_bold, FontWeight.Bold),
+    Font(R.font.gilroy_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.gilroy_regular, FontWeight.Normal),
+    Font(R.font.gilroy_thin, FontWeight.Thin),
+    Font(R.font.gilroy_light, FontWeight.Light),
+    Font(R.font.gilroy_ultra_light, FontWeight.ExtraLight),
+    Font(R.font.gilroy_semi_bold, FontWeight.SemiBold),
+    Font(R.font.gilroy_medium, FontWeight.Medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = gilroyFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 48.sp,
+        lineHeight = 29.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = gilroyFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 26.sp
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = gilroyFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = gilroyFontFamily,
+        fontWeight = FontWeight.Thin,
+        fontSize = 16.sp,
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = gilroyFontFamily,
+        fontWeight = FontWeight.Thin,
+        fontSize = 15.sp,
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = gilroyFontFamily,
+        fontWeight = FontWeight.Thin,
+        fontSize = 14.sp,
     )
-    */
+
+
 )
